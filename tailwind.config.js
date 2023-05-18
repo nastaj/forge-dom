a9a3d0372f846d2b2fc2fb2a63d0a05bb0f5d8c5
nastaj <G00424689@atu.ie>
2023-05-18 18:49:30 +0100

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "forge-gray": "#000004",
+        "forge-main": "#E32E2C",
+        "forge-accent": "#4EC5B7",
+      },
+    },
+  },
+  plugins: [],
+};
